30c09a418d86eea9406e7b60f2395546c83ae964
Davor <davor.hoic@gmail.com>
2024-04-21 22:35:22 +0200

    Prije refaktoringa

--- controllers/note.php
+++ controllers/note.php
@@ -5,7 +5,18 @@ $config = require 'config.php';
 $db = new Database($config['database']);
 
 $heading = "Note";
+$currentUserId=1;
 
-$note = $db->query("select * from notes where id = :id", ['id'=>$_GET['id']])->fetch();
+$note = $db->query("select * from notes where id = :id", [
+    'id' => $_GET['id']
+])->fetch();
+
+if (!$note) {
+    abort();
+}
+
+if (!$note <> $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
